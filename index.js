58fcf8d039d62298331ffde014b4623f65497040
noelensith <mbnoelen@gmail.com>
2022-03-21 18:59:25 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        //debugger;
+    }
+
+    return gifts;
+}
+wrapGifts(gifts);*/
+const cards = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(cards) {
+    //create empty array then push message to array while interpolate cards [array] 
+    //into suprise array, return array
+    const surprise = []
+    for (let i = 0; i < cards.length; i++)
+        surprise.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+    return surprise;
+
+}
+
+console.log(writeCards(cards))
+//use while loop 
+function countDown() {
+    let countdown = 10
+    while (countdown >= 0) {
+        console.log(countdown--)
+    }
+}
